1f6741d67de014841ff3227918c84ff8cd33bb1e
Mashrafi Bin Shoyeb <masrafirinvi@gmail.com>
2021-11-30 01:19:51 +0600

    first commit

---
+++ src/Components/Navigation/Navigation.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./Navigation.css";
+
+const Navigation = () => {
+  return (
+    <div>
+      <div className="header">
+        <nav>
+          <h3 className="navigation-header">Mashrafi</h3>
+        </nav>
+      </div>
+    </div>
+  );
+};
+
+export default Navigation;
